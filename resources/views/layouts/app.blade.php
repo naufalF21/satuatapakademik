89b3542a359a6a4d33628272358579d30514ce6c
naufalF21 <naufalfirman098@gmail.com>
2024-08-04 13:28:09 +0700

    remove breeze

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,11 @@
 </head>
 
 <body class="bg-base-white font-poppins">
+    {{-- wa floating icon --}}
+    <a href="https://api.whatsapp.com/send?phone=6285852115872&text=testing" class="float" target="_blank">
+        <i class="fa fa-whatsapp my-float"></i>
+    </a>
+    {{-- end wa floating icon --}}
     <div class="flex flex-col text-base">
         <x-navbar />
         <div>
--- routes/web.php
+++ routes/web.php
@@ -8,27 +8,9 @@ use App\Http\Controllers\ProgramsController;
 use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/about', [AboutController::class, 'index'])->name('about');
 Route::get('/student', [StudentController::class, 'index'])->name('student');
 Route::get('/programs', [ProgramsController::class, 'index'])->name('programs');
 Route::get('/programs/movie', [MovieProgramController::class, 'index'])->name('programs.movie');
 Route::get('/programs/movie/{id}', [MovieProgramController::class, 'detail'])->name('programs.movie.detail');
-
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
-
-// Route::middleware('auth')->group(function () {
-//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-// });
-
-
-
-// require __DIR__ . '/auth.php';
